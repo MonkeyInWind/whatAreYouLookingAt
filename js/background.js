b31ad70734385b39c1d37a7ea82a12a7b4d3fd8e
MonkeyInWind <xlyytcy_pyc@outlook.com>
2021-01-31 21:37:12 +0800

    1

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+chrome.runtime.onInstalled.addListener(function() {
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         console.log(request);
         console.log(sender);
@@ -10,4 +10,12 @@ window.onload = () => {
             message: '大哥你让人揍了'
         });
     });
-}
+
+    chrome.contextMenus.create({
+        "id": "contextMenu",
+        "title": "你瞅啥右键菜单",
+        onclick: () => {
+            console.log('右键点击了你瞅啥');
+        }
+    });
+});
